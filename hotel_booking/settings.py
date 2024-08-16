9ab4523e2336ba4d8d6a281a4907911b74da21a4
uzzal2200 <uzzalhasan1238@gmail.com>
2024-08-16 15:06:32 +0600

    add database

--- hotel_booking/settings.py
+++ hotel_booking/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import environ
 env = environ.Env()
 environ.Env.read_env()
@@ -81,11 +82,18 @@ WSGI_APPLICATION = 'hotel_booking.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://libraray_management_user:SOozQyMyC9sesXXxdFp6LbwwfmYZeXlV@dpg-cqr48clsvqrc73fpk28g-a.oregon-postgres.render.com/libraray_management',
+        
+    )
 }
 
 
